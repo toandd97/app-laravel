8a84e2d0a943f90f9f209dfe11f2b8f1cc3b5c34
toandd <toandd@mobio.io>
2024-08-20 17:09:50 +0700

    add admin view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+        return view('admin.index');
+    });
+    Route::get('/login', function () {
+        return view('admin.register');
+    });
+});
